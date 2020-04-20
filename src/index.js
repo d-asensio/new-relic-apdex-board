99995780d27c29d847e6d68595bc38341d645970
David Asensio Cañas <david.asensio@icloud.com>
2020-04-20 13:27:47 +0200

    add and cofigure lint-staged with husky

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+/* global fetch */
+
 import './styles/index.scss'
+
+;(async () => {
+  const res = await fetch('data/host-app-data.json')
+  const appRecords = await res.json()
+
+  console.log(appRecords)
+})()
 
 window.onload = () => {
   console.log('Hello world')
