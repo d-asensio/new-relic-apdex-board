b93bbdab02ccf2cd60270e65593d19c33ab27ebe
David Asensio Cañas <david.asensio@icloud.com>
2020-04-21 22:56:19 +0200

    add simple event bus implementation

---
+++ src/utils/event-bus.js
@@ -0,0 +1,69 @@
+export class EventBus {
+  constructor () {
+    this._events = new Map()
+  }
+
+  on (eventName, listener) {
+    this._isValidListenerOrThrow(listener)
+    this._registerEventListener(eventName, listener)
+
+    return () => {
+      this.off(eventName, listener)
+    }
+  }
+
+  off (eventName, listener) {
+    this._isValidListenerOrThrow(listener)
+    this._unregisterEventListener(eventName, listener)
+  }
+
+  async emit (eventName, ...eventData) {
+    await this._callEventListeners(eventName, eventData)
+  }
+
+  _registerEventListener (eventName, listener) {
+    const eventListeners = this._getOrCreateEventListeners(eventName)
+
+    eventListeners.add(listener)
+  }
+
+  _unregisterEventListener (eventName, listener) {
+    const eventListeners = this._getListenersOfEvent(eventName)
+
+    eventListeners.delete(listener)
+  }
+
+  async _callEventListeners (eventName, eventData) {
+    const eventListeners = this._getListenersOfEvent(eventName)
+
+    for (const listener of eventListeners) {
+      await (async function IIFE (closuredEventData) {
+        listener(...closuredEventData)
+      })(eventData)
+    }
+  }
+
+  _isValidListenerOrThrow (listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError(
+        'The event listener must be a function.'
+      )
+    }
+  }
+
+  _getOrCreateEventListeners (eventName) {
+    if (!this._hasEvent(eventName)) {
+      this._events.set(eventName, new Set())
+    }
+
+    return this._getListenersOfEvent(eventName)
+  }
+
+  _getListenersOfEvent (eventName) {
+    return this._events.get(eventName)
+  }
+
+  _hasEvent (eventName) {
+    return this._events.has(eventName)
+  }
+}
---
+++ tests/event-bus.test.js
@@ -0,0 +1,87 @@
+import { EventBus } from '../src/utils'
+
+describe('EventBus', () => {
+  it('is created', () => {
+    const eventBus = new EventBus()
+
+    expect(eventBus).toBeInstanceOf(EventBus)
+  })
+
+  it('throws an error if the passed listener to "eventBus.on()" is not a function', () => {
+    const eventBus = new EventBus()
+
+    expect(() => {
+      eventBus.on('invalidListener', null)
+    }).toThrowErrorMatchingSnapshot()
+  })
+
+  it('throws an error if the passed listener to "eventBus.off()" is not a function', () => {
+    const eventBus = new EventBus()
+
+    expect(() => {
+      eventBus.off('invalidListener', null)
+    }).toThrowErrorMatchingSnapshot()
+  })
+
+  it('calls attached listener', async () => {
+    const listener = jest.fn()
+    const eventBus = new EventBus()
+
+    eventBus.on('event', listener)
+    await eventBus.emit('event')
+
+    expect(listener.mock.calls.length).toBe(1)
+  })
+
+  it('calls attached listener with data', async () => {
+    const listener = jest.fn()
+    const eventBus = new EventBus()
+
+    const dataParam1 = {}
+    const dataParam2 = 'second paramenter'
+
+    eventBus.on('event', listener)
+    await eventBus.emit('event', dataParam1, dataParam2)
+
+    expect(listener.mock.calls[0][0]).toBe(dataParam1)
+    expect(listener.mock.calls[0][1]).toBe(dataParam2)
+  })
+
+  it('calls multiple attached listeners', async () => {
+    const listener = jest.fn()
+    const listener2 = jest.fn()
+    const eventBus = new EventBus()
+
+    eventBus.on('event', listener)
+    eventBus.on('event', listener2)
+
+    await eventBus.emit('event')
+
+    expect(listener.mock.calls.length).toBe(1)
+    expect(listener2.mock.calls.length).toBe(1)
+  })
+
+  it('do not call a attached event if unregistered trough the retuned unregister function', async () => {
+    const listener = jest.fn()
+    const eventBus = new EventBus()
+
+    const unregister = eventBus.on('event', listener)
+    unregister()
+
+    await eventBus.emit('event')
+
+    expect(listener.mock.calls.length).toBe(0)
+  })
+
+  it('do not call a attached event if unregistered trough "eventBus.off()"', async () => {
+    const listener = jest.fn()
+    const eventBus = new EventBus()
+
+    eventBus.on('event', listener)
+    eventBus.off('event', listener)
+
+    await eventBus.emit('event')
+
+    expect(listener.mock.calls.length).toBe(0)
+  })
+})
