455b8ed44456bd2522e86e3c1d07cc1a2d6ada6b
David Asensio Cañas <david.asensio@icloud.com>
2020-04-22 17:55:09 +0200

    mount the app to the html file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-/* global fetch */
 
 import './styles/index.scss'
 
-;(async () => {
-  const res = await fetch('data/host-app-data.json')
-  const appRecords = await res.json()
+import { DashboardController } from './controllers'
+import { DashboardService } from './services'
+import { DashboardView } from './views'
 
-  console.log(appRecords)
-})()
+const controller = new DashboardController(
+  new DashboardService(),
+  new DashboardView()
+)
 
-window.onload = () => {
-  const dashboardElement = document.querySelector('.Dashboard')
-  const toggleElement = document.querySelector('.Toggle > input')
-
-  toggleElement.addEventListener('change', () => {
-    const isInListLayout = dashboardElement.classList.contains('Dashboard--listLayout')
-
-    if (isInListLayout) {
-      dashboardElement.classList.remove('Dashboard--listLayout')
-    } else {
-      dashboardElement.classList.add('Dashboard--listLayout')
-    }
-  })
+function renderApp () {
+  const appRoot = document.getElementById('app')
+  controller.renderToDom(appRoot)
 }
+
+;(async function IIFE () {
+  await controller.init()
+
+  if (document.readyState === 'complete') {
+    renderApp()
+  } else {
+    document.addEventListener(
+      'DOMContentLoaded',
+      () => renderApp()
+    )
+  }
+})()
